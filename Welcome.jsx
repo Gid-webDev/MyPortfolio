9e893ddb299d3fb8953aea142e278402b2306951
Gideon <117660758+Gid-webDev@users.noreply.github.com>
2023-09-11 13:16:58 +0000

    Add files via upload

---
+++ Welcome.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import {BiChevronsRight} from 'react-icons/bi'
+
+const Welcome = () => {
+    const [showWelcome, setWelcome] = useState(true);
+
+
+    const WelcomeOverlay = {width:'100%', height:'100%', position:'absolute',
+     backgroundColor:'rgba(21, 21, 46, 0.4)', top:'0', zIndex:'6', transition:'0.9s',
+     backdropFilter:' blur(7px)', 
+    }
+    const WelcomeContainer = {height:'100vh', display:'grid', placeItems:'center', 
+    textAlign:'center',
+    }
+    const ProfilePix = {
+      borderRadius:'50% 20px', width:'100%', boxShadow:'7px 8px rgb(255, 255, 255, 0.25)',
+      position:'relative',
+  }
+  const ProfileOverlay = {
+    backgroundColor:' rgba(21, 21, 46, 0.3)', position:'absolute', height:'100%', 
+    width:'100%', zIndex:'1', top:'0', left:'0', display:'none'
+}
+  const NameContainer = {
+    color:'#ddd', textAlign:'start', padding:'0 20px', position:'relative',
+    zIndex:'10'
+  }
+  const Names = {
+    fontSize:'35px', fontWeight:'700', textShadow:'3px 3px rgba(255, 255 255, 1)',
+  }
+  const Title = {
+    fontSize:'28px', fontWeight:'100'
+  }
+  const NaijaFlag = {
+    width:'30px', height:'30px', borderRadius:'50%'
+  }
+  const Nigerian = {
+    fontSize:'16px', color:'#ddd'
+}
+const Btn = {
+  width:'180px', padding:'10px 14px', fontSize:'16px', color:'#111', position:'relative',
+  backgroundColor:'#ddd', border:'none', borderRadius:'5px', 
+  display:'flex', alignItems:'center', justifyContent:'space-between', zIndex:'10'
+}
+
+
+
+  return (
+    <>
+      {showWelcome&& <div style={WelcomeOverlay}>
+       <div className='' style={WelcomeContainer}>
+       <figure style={ProfileOverlay}></figure>
+        <section className='cardContainer row g-0' style={{maxWidth:'800px', maxHeight:'70vh', borderRadius:'20px', scale:'0.8', top:'-40px', position:'relative'}}>
+        <div className='col-sm-4' style={{scale:''}}>
+         <img style={ProfilePix}
+         src={('gid.jpg')} alt=''/>
+        </div>
+        <div className='col-sm-8' style={NameContainer}>
+          <div style={Names}>
+           Gideon N. Nwabueze
+           <span>
+            <div style={Title}>
+             FrontEnd Developer 
+             <span style={{display:'flex'}}>
+              <img src={('9ja.png')} alt='Nigerian flag'
+               style={NaijaFlag}/>
+              <p className='px-2' style={Nigerian}> 
+               Proudly Nigerian
+              </p>
+             </span>
+            </div>
+           </span>
+          </div>
+          
+          <button style={Btn} 
+           onClick={() => setWelcome(false)}>
+           Continue to Page 
+           <BiChevronsRight style={{fontSize:'20px'}}/>
+        </button>
+        </div>
+        
+         
+        
+        
+        </section>
+       </div>
+      
+      </div>}
+    </>
+  )
+}
+
+export default Welcome
