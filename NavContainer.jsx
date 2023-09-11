482f9e26724177199a1570e7088599155b34595d
Gideon <117660758+Gid-webDev@users.noreply.github.com>
2023-09-11 13:20:13 +0000

    Add files via upload

--- NavContainer.jsx
+++ NavContainer.jsx
@@ -7,12 +7,17 @@ import {ImLocation2} from 'react-icons/im';
 
 const NavContainer = ({setShowDp}) => {
     
-  const socialStyle = {display: 'flex', justifyContent:'flex-end'}
+  const socialStyle = {
+    display: 'flex', justifyContent:'flex-end'
+  }
+  const Social_a = {
+    color:'rgb(23, 23, 73)', scale:'1.2', marginLeft:'25px',
+  }
   return (
     <div>
 
     <nav className="container" 
-    style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+    style={{display:'flex', justifyContent:'space-between', alignItems:'center',}}>
       {/*Brand image*/}
     <div className='bg-dark'style={{width:'42px', height:'42px', borderRadius:'50%', cursor:'pointer'}}>
       <img src={('dp.JPG')} alt='displayPic' className='img-fluid w-100 h-100 rounded-circle'
@@ -24,19 +29,19 @@ const NavContainer = ({setShowDp}) => {
     </div>
 
      <div className="socials" style={socialStyle}>
-      <a className="navbar" target='about_blank' href="https://www.github.com/Gid-webDev">
+      <a className="navbar" style={Social_a} target='about_blank' href="https://www.github.com/Gid-webDev">
         <BsGithub/>
       </a>
-      <a className="navbar" target='about_blank' href="https://www.linkedin.com/in/gideon-nwabueze-83840b281/">
+      <a className="navbar" style={Social_a} target='about_blank' href="https://www.linkedin.com/in/gideon-nwabueze-83840b281/">
         <FaLinkedinIn/>
       </a>  
-      <a className="navbar" target='about_blank' href="https://twitter.com/gid2gid?s=11&t=GbRBhIv6Ws3cplWUbCrU4Q">
+      <a className="navbar" style={Social_a} target='about_blank' href="https://twitter.com/gid2gid?s=11&t=GbRBhIv6Ws3cplWUbCrU4Q">
         <BsTwitter/>
       </a>
-      <a className="navbar" target='about_blank' href="https://web.facebook.com/Gidddy">
+      <a className="navbar" style={Social_a} target='about_blank' href="https://web.facebook.com/Gidddy">
         <FaFacebookF/>
       </a>
-      <a className="navbar" target='about_blank' href="https://secure.skype.com/portal/overview">
+      <a className="navbar" style={Social_a} target='about_blank' href="https://secure.skype.com/portal/overview">
         <BsSkype/>
       </a>
     </div>
